3fa5c361728a98b5063b205199784c3732ea6ad7
Kako Botasso <kakodev@gmail.com>
2017-03-24 22:02:38 -0300

    Adiciona acelerometro

--- MoviesLib/SettingsViewController.swift
+++ MoviesLib/SettingsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 enum SettingsType: String {
     case colorscheme = "colorscheme"
@@ -17,6 +18,10 @@ class SettingsViewController: UIViewController {
 
     @IBOutlet weak var scColorScheme: UISegmentedControl!
     @IBOutlet weak var swAutoPlay: UISwitch!
+    @IBOutlet weak var ivBG: UIImageView!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    var motionManager = CMMotionManager()
     
     override var canBecomeFirstResponder: Bool {
         return true
@@ -24,6 +29,17 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.startDeviceMotionUpdates(to: OperationQueue.main, withHandler: { (data: CMDeviceMotion?, error: Error?) in
+                if error == nil {
+                    if let data = data {
+                        let angle = atan2(data.gravity.x, data.gravity.y)
+                        self.ivBG.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
+                    }
+                }
+            })
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
