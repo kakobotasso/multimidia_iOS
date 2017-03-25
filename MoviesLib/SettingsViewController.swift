78a206febae8d7271df6b78f75e0ba58f9a152cf
Kako Botasso <kakodev@gmail.com>
2017-03-24 22:42:29 -0300

    Adicionando picker

--- MoviesLib/SettingsViewController.swift
+++ MoviesLib/SettingsViewController.swift
@@ -19,9 +19,17 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var scColorScheme: UISegmentedControl!
     @IBOutlet weak var swAutoPlay: UISwitch!
     @IBOutlet weak var ivBG: UIImageView!
-    @IBOutlet weak var pickerView: UIPickerView!
+    
+    @IBOutlet weak var tfFood: UITextField!
+    var pickerView : UIPickerView!
     
     var motionManager = CMMotionManager()
+    var dataSource = [
+        "Arroz",
+        "Feijão",
+        "Ovo",
+        "Macarrão"
+    ]
     
     override var canBecomeFirstResponder: Bool {
         return true
@@ -29,6 +37,23 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pickerView = UIPickerView()
+        pickerView.backgroundColor = .white
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 44))
+            
+        let btCancel = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
+        let btSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let btDone = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        
+        toolbar.items = [btCancel, btSpace, btDone]
+        
+        tfFood.inputView = pickerView
+        tfFood.inputAccessoryView = toolbar
         
         if motionManager.isDeviceMotionAvailable {
             motionManager.startDeviceMotionUpdates(to: OperationQueue.main, withHandler: { (data: CMDeviceMotion?, error: Error?) in
@@ -40,6 +65,17 @@ class SettingsViewController: UIViewController {
                 }
             })
         }
+        
+//        pickerView.selectedRow(inComponent: 0)
+    }
+    
+    func cancel(){
+        tfFood.resignFirstResponder()
+    }
+    
+    func done(){
+        tfFood.text = dataSource[pickerView.selectedRow(inComponent: 0)]
+        cancel()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -66,6 +102,27 @@ extension SettingsViewController: UITextFieldDelegate {
     }
 }
 
+extension SettingsViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dataSource[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //print("acabaram de comer \(dataSource[pickerView.selectedRow(inComponent: component)])")
+        
+//        tfFood.text = dataSource[row]
+    }
+}
+
+extension SettingsViewController: UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataSource.count
+    }
+}
 
 
 
