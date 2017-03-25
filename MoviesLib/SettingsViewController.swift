b691f7f195e4aa697818f38b3e63a1615bc81e57
Usuário Convidado <Guest@PA06MICRO11.local>
2017-03-24 21:31:40 -0300

    Trabalha com audio e video

---
+++ MoviesLib/SettingsViewController.swift
@@ -0,0 +1,57 @@
+//
+//  SettingsViewController.swift
+//  MoviesLib
+//
+//  Created by Usuário Convidado on 22/03/17.
+//  Copyright © 2017 EricBrito. All rights reserved.
+//
+
+import UIKit
+
+enum SettingsType: String {
+    case colorscheme = "colorscheme"
+    case autoplay = "autoplay"
+}
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var scColorScheme: UISegmentedControl!
+    @IBOutlet weak var swAutoPlay: UISwitch!
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.becomeFirstResponder()
+    }
+    
+    @IBAction func changeColorScheme(_ sender: UISegmentedControl) {
+        UserDefaults.standard.set(sender.selectedSegmentIndex, forKey: SettingsType.colorscheme.rawValue)
+    }
+    
+    @IBAction func changeAutoplay(_ sender: UISwitch) {
+        UserDefaults.standard.set(sender.isOn, forKey: SettingsType.autoplay.rawValue)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        scColorScheme.selectedSegmentIndex = UserDefaults.standard.integer(forKey: SettingsType.colorscheme.rawValue)
+        swAutoPlay.setOn(UserDefaults.standard.bool(forKey: SettingsType.autoplay.rawValue), animated: true)
+    }
+    
+}
+
+extension SettingsViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+    }
+}
+
+
+
+
+
+
